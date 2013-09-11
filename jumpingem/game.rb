7f1ab77e8bd40072f916d8fbcafece38ee3a8493
Patrick Huesler <patrick.huesler@gmail.com>
2013-09-11 11:09:35 +0200

    hack in a white floor

--- jumpingem/game.rb
+++ jumpingem/game.rb
@@ -9,7 +9,7 @@ class Game < Window
     super(640, 480, false)
     self.caption = "Jump 'n Run"
     @standing, @walk1, @walk2, @jump = *Image.load_tiles(self, "sprites.png", 100, 160, false)
-    @x, @y = 200, 200
+    @x, @y = 400, 0
     @vy = 0
     @dir = :left
     @cur_image = @standing
@@ -45,6 +45,7 @@ class Game < Window
     # By adding 1 each frame, and (ideally) adding vy to y, the player's
     # jumping curve will be the parabole we want it to be.
     @vy += 1
+
     # Vertical movement
     if @vy > 0 && @y < 300 then
       @vy.times { @y += 1 }
@@ -56,6 +57,10 @@ class Game < Window
   end
 
   def draw
+    #draw_quad(x1, y1, c1, x2, y2, c2, x3, y3, c3, x4, y4, c4, z = 0, mode = :default)
+    # points are in clockwise order
+    draw_quad 0, 400, Color::WHITE, 640, 400, Color::WHITE, 640, 500, Color::WHITE, 0, 500, Color::WHITE
+
     if @dir == :left then
       offs_x = -25
       factor = 1.0
